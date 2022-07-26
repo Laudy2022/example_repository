7f703a84fd0428f3b7c41c7001c57fd9b8798e8f
Kimberlee Latondress <macbaby@kimberlees-air.attlocal.net>
2022-07-26 13:28:37 -0400

    finished loop practice

---
+++ module1/homework/loops/loops.js
@@ -0,0 +1,41 @@
+//  for(let i = 1; i <= 99
+//      ; i++) {
+//         console.log(i);
+//     } 
+
+// 1-99 ascending
+
+// for(let i = 99; i > 0; i--) {
+//   console.log(i);
+//  }
+ // 99-1 descending
+
+
+// for(let i = 1; i <= 99; i++) {
+     // const isEven = i % 2 === 0;
+     // if(isEven) {
+     //    console.log(i);
+     // }
+  //  }
+
+  //1-98 ascending, even only
+
+//   for(let i =1; i < 99; i++) {
+//    // % - (modulo) gives the remainder - note from class
+//        const isOdd = i % 2 === 1;
+//        if(isOdd) {
+//        console.log(i);
+//        }
+//      }
+//1-99 ascending, odd only
+
+// for(let i =99; i > 0; i--) {
+  
+//             const isOdd = i % 2 === 1;
+//            if(isOdd) {
+//            console.log(i);
+//            }
+//           }
+
+
+// 99-1 descending, odd only

1d86a307face2f723b09be6a6bed8009716668de
Kimberlee Latondress <macbaby@kimberlees-air.attlocal.net>
2022-07-30 07:26:57 -0400

    FizzBuzz complete

--- hw_main/w2d2answershw.js
+++ hw_main/w2d2answershw.js
@@ -25,11 +25,37 @@
 
 
 
-////////////////////////////////
+///////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
 
+for(let i = 1; i <= 100; i++) {
+    if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else if (i % 15 == 0) console.log("FizzBuzz");
+    console.log(i);
+  }
 
+
+
+
+// const grade= 93
+
+
+// if(grade >= 90) {
+// console.log("A")
+
+// } else if(grade >= 80) {
+//     console.log("B")
+
+// }else if(grade >= 70){
+//     console.log("C")
+
+// } else if(grade >= 55){
+//     console.log("D")
+// } else {
+//     console.log("F")
+// }
 
 
 
